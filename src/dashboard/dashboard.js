2d3aeeb41b5aa7f767c9de7caca267cfcf829b42
Mradul Dixit <mraduldixit2002@gmail.com>
2023-03-17 16:15:45 +0530

    fc

---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,22 @@
+import styles from "./dashboard.module.css";
+import { useNavigate } from "react-router-dom";
+
+export function Dashboard() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <center>
+        <h3 className={styles.heading}>Banking Application</h3>
+      </center>
+
+      <div className={styles.dashCont}>
+        <div onClick={() => navigate("/new")}>New Customer</div>
+        <div onClick={() => navigate("/deposit")}>Deposit</div>
+        <div onClick={() => navigate("/withdraw")}>Withdraw</div>
+        <div onClick={() => navigate("/transfer")}>Transfer</div>
+        <div onClick={() => navigate("/balance")}>Balance</div>
+      </div>
+    </div>
+  );
+}
